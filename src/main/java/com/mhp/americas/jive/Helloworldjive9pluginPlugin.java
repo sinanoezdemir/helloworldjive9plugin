10c1550b004c131b86f4ebeea595db86c4b1c42e
Sinan Özdemir <soezdemir@Sinans-MacBook-Pro.local>
2017-05-22 12:59:58 -0400

    First commit

---
+++ src/main/java/com/mhp/americas/jive/Helloworldjive9pluginPlugin.java
@@ -0,0 +1,11 @@
+package com.mhp.americas.jive;
+
+import com.jivesoftware.base.plugin.Plugin;
+
+public class Helloworldjive9pluginPlugin implements Plugin {
+    public void initPlugin() {
+    }
+
+    public void destroy() {
+    }
+}
